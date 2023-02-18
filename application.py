fd0c35f9f76264df31414cc7dc7e2ae68eee9ab2
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2023-02-18 17:18:34 -0600

    add routes, error handling

--- application.py
+++ application.py
@@ -10,7 +10,23 @@ def pull_feed():
 
 @application.route('/', methods=['GET', 'POST'])
 def main():
-    return 'hello world'
+    items = pull_feed()
+    length = len(items)
+    return(render_template('main.html', items=items, length=length))
 
-# if __name__ == "__main__":
-#     application.run( debug=True)
+@application.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+@application.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+@application.errorhandler(500)
+def internal_error(e):
+    return render_template("broken.html"), 500
+
+if __name__ == "__main__":
+    application.run(debug=True)
