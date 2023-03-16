6d53d2cd8848c29b6332000bd7380cfc407cd71c
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2023-03-16 17:06:41 -0500

    add email function
    adding ability to send me an email

---
+++ mail.py
@@ -0,0 +1,20 @@
+from email.mime.text import MIMEText
+import os
+import smtplib
+
+def send_email(body):
+    sender = "audiobatonmusic@gmail.com"
+    # password = "rorywasszzghjvlp"
+    recipients = ["fertilityhollis88@gmail.com"]
+    password = os.environ['email_pass']
+    
+    msg = MIMEText(body)
+    msg['Subject'] = "WPLN Newscast Comment"
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtp_server.login(sender, password)
+    smtp_server.sendmail(sender, recipients, msg.as_string())
+    smtp_server.quit()
+
+send_email(body='hmmmmmmmmmmmmmmm does this work? cannot quite tell')
