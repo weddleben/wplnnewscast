7eaeb31487ad7adb85f40cd93e491db11fa207ec
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2023-03-16 18:53:40 -0500

    contact us!
    it works!

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory, Request
 
 from rss import get_items, check_banner
+from mail import send_email
 
 application = Flask(__name__)
 
@@ -41,7 +42,23 @@ def admin():
             return render_template('admin.html', emoji='&#128078;')
 
     return render_template('admin.html')
-    
+
+@application.route('/contact/', methods=['GET', 'POST'] )    
+def contact():
+    if request.method == 'POST':
+
+        email_address = request.form['email_address']
+        if email_address == '':
+            email_address = 'not provided'
+
+        email_message = request.form['email_message']
+
+        to_send = f'Email Address: {email_address}\nEmail Message: {email_message}'
+        send_email(body=to_send)
+
+        return render_template('post_contact.html', emoji='&#128077;')
+
+    return render_template('contact.html')
 
 
 @application.errorhandler(404)
@@ -53,4 +70,4 @@ def internal_error(e):
     return render_template("broken.html"), 500
 
 if __name__ == "__main__":
-    application.run()
+    application.run(debug=True)
