c069988b5404fcb5bb84f649c962e5063b221698
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2023-03-19 18:51:01 -0500

    more tests
    honestly, these feel kind of pointless...

--- test_misc.py
+++ test_misc.py
@@ -39,6 +39,56 @@ def test_admin(client):
     assert resp.status_code == 200 # redirect
 
 '''
+post request to valid post URL(s)
+'''
+def test_contact_post1(client):
+    '''form data required for post request'''
+    resp = client.post('/contact/', data={"email_address": "some email","email_message": "some message",})
+    assert resp.status_code == 200
+
+def test_contact_post2(client):
+    '''should still work if email_address is empty.
+    '''
+    resp = client.post('/contact/', data={"email_address": "","email_message": "some message",})
+    assert resp.status_code == 200
+
+def test_contact_post3(client):
+    '''an empty email_message should also come through fine. we're only doing validation on the front end.'''
+    resp = client.post('/contact/', data={"email_address": "","email_message": "",})
+    assert resp.status_code == 200
+
+def test_contact_post4(client):
+    '''should get 405 if form data is missing'''
+    resp = client.post('/contact/')
+    assert resp.status_code == 400
+
+def test_admin_post1(client):
+    '''this is NOT testing whether the credentials are correct! only if the post request works'''
+    resp = client.post('/admin/', data={"user": "","message": "",})
+    assert resp.status_code == 200
+
+def test_admin_post2(client):
+    '''should fail if form data is missing'''
+    resp = client.post('/admin/')
+    assert resp.status_code == 400
+
+'''
+post request to non post URL(s)
+'''
+
+def test_about_post(client):
+    resp = client.post('/about/')
+    assert resp.status_code == 405
+
+def test_podcast_post(client):
+    resp = client.post('/podcast/')
+    assert resp.status_code == 405
+
+def test_podcastrss_post(client):
+    resp = client.post('/podcastrss/')
+    assert resp.status_code == 405
+
+'''
 fake URL(s)
 '''
 
