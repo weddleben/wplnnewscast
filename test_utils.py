69368b0821c6e4582c916645f14946b812591515
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2023-06-04 22:13:41 -0500

    add test file, add tests

---
+++ test_utils.py
@@ -0,0 +1,15 @@
+import xml.etree.ElementTree as ET
+
+from rss import get_feed, get_items
+
+
+def test_get_items():
+    assert type(get_items()) == list
+
+def test_get_items_2():
+    items = get_items()
+    for item in items:
+        assert type(item) == dict
+
+def test_get_feed():
+    assert isinstance(get_feed(), ET.Element)
